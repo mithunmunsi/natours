b69a4f9ca3291716b86dae8db0ba803aebc1551f
Mithun Munsi <mithun.munsi@gmail.com>
2023-09-30 20:59:27 +0300

    new files added

---
+++ app.js
@@ -0,0 +1,22 @@
+// Adding Express.js
+const express = require('express');
+
+const app = express();
+
+// Creating Routes
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+
+// Creating Port
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port: ${port}`);
+});
